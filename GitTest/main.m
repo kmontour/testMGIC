6d1a79f77aa032ce6813c04e0e8cd3447d06c0a4
Kaitlyn Barbara Montour <kmontour@ccl-gmlw177-x11.uits.uwm.edu>
2016-03-07 18:35:50 -0600

    Initial Commit

---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Kaitlyn Barbara Montour on 3/7/16.
+//  Copyright (c) 2016 Kaitlyn Barbara Montour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
